14b651980d8c30af4e6b20dcc7c1a47676dd492d
Ashley Moore <mooream2291@gmail.com>
2020-10-07 17:58:40 -0400

    added missing code

--- js/app.js
+++ js/app.js
@@ -1,32 +1,63 @@
-'use strict'
+'use strict';
 
 let imageArr = [];
-
-$(document).ready(function() {
-
-  $.ajax('data/page-1.json')
-    .then (getHorn => {
-//creature is naming each individual object//
-      getHorn.forEach((creature) => {
-        new Horn(creature).render();
-      });
-    });
-});
 
 Horn.prototype.render = function() {
 
   //html is a native function//
 
   const template = $('#photo-template').html();
-  const newSection = $('<section></section>');
+  const newSection = $(`<section class= ${this.keyword}></section>`);
   newSection.html(template);
   //find is a native function to target an html element//
   newSection.find('h2').text(this.title);
   newSection.find('img').attr('src', this.image_url);
-  newSection.find('p').text(this.descirption);
+  newSection.find('p').text(this.description);
 
   $('main').append(newSection);
 };
+
+$(document).ready(function() {
+
+  $.ajax('data/page-1.json')
+    .then (getHorn => {
+      //creature is naming each individual object//
+      getHorn.forEach((creature) => {
+        new Horn(creature).render();
+      });
+      keyword();
+      dropdown();
+      $('.remove').hide();
+    });
+});
+let keywordArr = [];
+
+function keyword() {
+  imageArr.forEach(value => {
+    //for each item in the image array, if it DOES NOT include the keyword, then run this function//
+    if (!keywordArr.includes(value.keyword)) {
+      keywordArr.push(value.keyword);
+    };
+
+  });
+};
+
+function dropdown() {
+  let click = $('#sort');
+  // empty is a native js function//
+  click.empty();
+  const option = $('#option');
+  keywordArr.forEach(keyword => {
+    let sort = $(`<option class=${keyword} value=${keyword}>${keyword}</option>`);
+    option.append(sort);
+  });
+}
+$('select').on('change',(event) => {
+  let newval = event.target.value;
+  console.log(newval);
+  $('section').hide();
+  $(`.${newval}`).show();
+});
 
 function Horn (horn) {
   this.image_url = horn.image_url;
@@ -36,4 +67,5 @@ function Horn (horn) {
   this.horns = horn.horns;
   imageArr.push(this);
 }
-  
+
+
